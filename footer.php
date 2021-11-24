bc2e372f75658e94c3fe4142507f30d60bab7c05
Juan Gutiérrez Calquín <juan.gutierrezcalquin@gmail.com>
2021-11-24 02:25:00 -0300

    80
    cambios  24-11-2021

---
+++ footer.php
@@ -0,0 +1,95 @@
+
+
+
+<div class="footer">
+
+<div class="container">
+
+
+
+<div class="row">
+            <div class="col-lg-3 col-sm-3">
+                   <h4>Informacion</h4>
+                   <ul class="row">
+                <li class="col-lg-12 col-sm-12 col-xs-3"><a href="about.php">Nosotros</a></li>
+                <li class="col-lg-12 col-sm-12 col-xs-3"><a href="agents.php">Agentes</a></li>         
+                <li class="col-lg-12 col-sm-12 col-xs-3"><a href="blog.php">Blog</a></li>
+                <li class="col-lg-12 col-sm-12 col-xs-3"><a href="contact.php">Contactenos</a></li>
+              </ul>
+            </div>
+            
+            <div class="col-lg-3 col-sm-3">
+                    <h4>Newsletter</h4>
+                    <p>Informese de la ultimas propiedades en venta.</p>
+                    <form class="form-inline" role="form">
+                            <input type="text" placeholder="Ingrese su email" class="form-control">
+                                <button class="btn btn-success" type="button">Envieme ofertas</button></form>
+            </div>
+            
+            <div class="col-lg-3 col-sm-3">
+                    <h4>Siganos en:</h4>
+                    <a href="#"><img src="images/facebook.png" alt="facebook"></a>
+                    <a href="#"><img src="images/twitter.png" alt="twitter"></a>
+                    <a href="#"><img src="images/linkedin.png" alt="linkedin"></a>
+                    <a href="#"><img src="images/instagram.png" alt="instagram"></a>
+            </div>
+
+             <div class="col-lg-3 col-sm-3">
+                    <h4>Contactenos </h4>
+                    <!-- <p><b>Bootstrap Realestate Inc.</b><br>
+<span class="glyphicon glyphicon-map-marker"></span> 8290 Walk Street, Australia <br> -->
+<span class="glyphicon glyphicon-envelope"></span> info@rpropiedades.cl<br>
+<span class="glyphicon glyphicon-earphone"></span> +569 9156 3334</p>
+            </div>
+        </div>
+<p class="copyright">Copyright 2021. Todos los derechos reservados.	</p>
+
+
+</div></div>
+
+
+
+
+<!-- Modal -->
+<div id="loginpop" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="row">
+        <div class="col-sm-6 login">
+        <h4>Login</h4>
+          <form class="" role="form">
+        <div class="form-group">
+          <label class="sr-only" for="exampleInputEmail2">Email</label>
+          <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Ingresa email">
+        </div>
+        <div class="form-group">
+          <label class="sr-only" for="exampleInputPassword2">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+        </div>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox"> Recuerdame
+          </label>
+        </div>
+        <button type="submit" class="btn btn-success">Ingresar</button>
+      </form>          
+        </div>
+        <div class="col-sm-6">
+          <h4>Registro de Nuevos Usuarios</h4>
+          <p>Únase hoy y manténgase actualizado con todas las ofertas de propiedades que están sucediendo.</p>
+          <button type="submit" class="btn btn-info"  onclick="window.location.href='register.php'">Únete ahora</button>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /.modal -->
+
+
+
+</body>
+</html>
+
+
+
---
+++ index.php
@@ -0,0 +1,289 @@
+<?php include'header.php';?>
+
+<div class="">
+    
+
+            <div id="slider" class="sl-slider-wrapper">
+
+        <div class="sl-slider">
+        
+          <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+            <div class="sl-slide-inner">
+              <div class="bg-img bg-img-1"></div>
+              <h2><a href="#">Ventas, Compras y Arriendos</a></h2>
+              <blockquote>              
+              <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
+              <p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p>
+              <cite>$ 20,000,000</cite>
+              </blockquote>
+            </div>
+          </div>
+          
+          <div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+            <div class="sl-slide-inner">
+              <div class="bg-img bg-img-2"></div>
+              <h2><a href="#">Asesoria Inmobilaria</a></h2>
+              <blockquote>              
+              <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
+              <p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p>
+              <cite>$ 20,000,000</cite>
+              </blockquote>
+            </div>
+          </div>
+          
+          <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
+            <div class="sl-slide-inner">
+              <div class="bg-img bg-img-3"></div>
+              <h2><a href="#">Tramites ante el conservador de bienes raices</a></h2>
+              <blockquote>              
+              <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
+              <p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p>
+              <cite>$ 20,000,000</cite>
+              </blockquote>
+            </div>
+          </div>
+          
+          <div class="sl-slide" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
+            <div class="sl-slide-inner">
+              <div class="bg-img bg-img-4"></div>
+              <h2><a href="#">Tasaciones</a></h2>
+              <blockquote>              
+              <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
+              <p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p>
+              <cite>$ 20,000,000</cite>
+              </blockquote>
+            </div>
+          </div>
+          
+          <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
+            <div class="sl-slide-inner">
+              <div class="bg-img bg-img-5"></div>
+              <h2><a href="#">Estudio de Titulos</a></h2>
+              <blockquote>              
+              <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
+              <p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p>
+              <cite>$ 20,000,000</cite>
+              </blockquote>
+            </div>
+          </div>
+        </div><!-- /sl-slider -->
+
+
+
+        <nav id="nav-dots" class="nav-dots">
+          <span class="nav-dot-current"></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </nav>
+
+      </div><!-- /slider-wrapper -->
+</div>
+
+
+
+<div class="banner-search">
+  <div class="container"> 
+    <!-- banner -->
+    <h3>Compra, Venta y Arriendo</h3>
+    <div class="searchbar">
+      <div class="row">
+        <div class="col-lg-6 col-sm-6">
+          <input type="text" class="form-control" placeholder="Busqueda de propiedades">
+          <div class="row">
+            <div class="col-lg-3 col-sm-3 ">
+              <select class="form-control">
+                <option>Compra</option>
+                <option>Arriendo</option>
+                <option>Venta</option>
+              </select>
+            </div>
+            <div class="col-lg-3 col-sm-4">
+              <select class="form-control">
+                <option>Precio</option>
+                <option>700 UF - 1200 UF</option>
+                <option>1201 UF - 1500 UF </option>
+                <option>1501 UF - 3000 UF</option>
+                <option>3001 UF - Más</option>
+              </select>
+            </div>
+            <div class="col-lg-3 col-sm-4">
+            <select class="form-control">
+                <option>Sitio</option>
+                <option>Departamento</option>
+                <option>Casa</option>
+                <option>Oficina</option>
+                <option>Bodega</option>
+              </select>
+              </div>
+              <div class="col-lg-3 col-sm-4">
+              <button class="btn btn-success"  onclick="window.location.href='buysalerent.php'">Buscar</button>
+              </div>
+          </div>
+          
+          
+        </div>
+        <div class="col-lg-5 col-lg-offset-1 col-sm-6 ">
+          <p>Únase ahora y manténgase actualizado con todas las ofertas de propiedades.</p>
+          <button class="btn btn-info"   data-toggle="modal" data-target="#loginpop">Login</button></div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- banner -->
+<div class="container">
+  <div class="properties-listing spacer"> <a href="buysalerent.php" class="pull-right viewall">Ver la lista completa</a>
+    <h2>Propiedades Destacadas</h2>
+    <div id="owl-example" class="owl-carousel">
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
+          <div class="status sold">Vendida</div>
+        </div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/2.jpg" class="img-responsive" alt="properties"/>
+          <div class="status new">NUevo</div>
+        </div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/3.jpg" class="img-responsive" alt="properties"/></div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/4.jpg" class="img-responsive" alt="properties"/></div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
+          <div class="status sold">Vendida</div>
+        </div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/2.jpg" class="img-responsive" alt="properties"/>
+          <div class="status sold">Vendida</div>
+        </div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/3.jpg" class="img-responsive" alt="properties"/>
+          <div class="status new">New</div>
+        </div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/4.jpg" class="img-responsive" alt="properties"/></div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/1.jpg" class="img-responsive" alt="properties"/></div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/2.jpg" class="img-responsive" alt="properties"/></div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      <div class="properties">
+        <div class="image-holder"><img src="images/properties/3.jpg" class="img-responsive" alt="properties"/></div>
+        <h4><a href="property-detail.php">Propiedades</a></h4>
+        <p class="price">Price: $234,900</p>
+        <div class="listing-detail"><span data-toggle="tooltip" data-placement="bottom" data-original-title="Bed Room">5</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Living Room">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Parking">2</span> <span data-toggle="tooltip" data-placement="bottom" data-original-title="Kitchen">1</span> </div>
+        <a class="btn btn-primary" href="property-detail.php">Ver Detalles</a>
+      </div>
+      
+    </div>
+  </div>
+  <div class="spacer">
+    <div class="row">
+      <div class="col-lg-6 col-sm-9 recent-view">
+        <!-- <h3>Acerca de Nosotros</h3>
+        <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.<br><a href="about.php">Learn More</a></p>
+       -->
+      </div>
+      <div class="col-lg-5 col-lg-offset-1 col-sm-3 recommended">
+        <h3>Propiedades Recomendadas</h3>
+        <div id="myCarousel" class="carousel slide">
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+            <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+            <li data-target="#myCarousel" data-slide-to="3" class=""></li>
+          </ol>
+          <!-- Carousel items -->
+          <div class="carousel-inner">
+            <div class="item active">
+              <div class="row">
+                <div class="col-lg-4"><img src="images/properties/1.jpg" class="img-responsive" alt="properties"/></div>
+                <div class="col-lg-8">
+                  <h5><a href="property-detail.php">Oferta</a></h5>
+                  <p class="price">$300,000</p>
+                  <a href="property-detail.php" class="more">Mas Detalles</a> </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="row">
+                <div class="col-lg-4"><img src="images/properties/2.jpg" class="img-responsive" alt="properties"/></div>
+                <div class="col-lg-8">
+                  <h5><a href="property-detail.php">Oferta</a></h5>
+                  <p class="price">$300,000</p>
+                  <a href="property-detail.php" class="more">Mas Detalles</a> </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="row">
+                <div class="col-lg-4"><img src="images/properties/3.jpg" class="img-responsive" alt="properties"/></div>
+                <div class="col-lg-8">
+                  <h5><a href="property-detail.php">Oferta</a></h5>
+                  <p class="price">$300,000</p>
+                  <a href="property-detail.php" class="more">Mas Detalles</a> </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="row">
+                <div class="col-lg-4"><img src="images/properties/4.jpg" class="img-responsive" alt="properties"/></div>
+                <div class="col-lg-8">
+                  <h5><a href="property-detail.php">Oferta</a></h5>
+                  <p class="price">$300,000</p>
+                  <a href="property-detail.php" class="more">Mas Detalles</a> </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include'footer.php';?>
